920f4b0e7ccdb97d7cef13778f311c10dc08db65
seandurnin-cs <sdurnin144@gmail.ie>
2026-06-22 14:07:16 +0100

    disabled old TrainObservationController

--- src/main/java/dev/slimtom/an_traein/controller/TrainObservationController.java
+++ src/main/java/dev/slimtom/an_traein/controller/TrainObservationController.java
@@ -1,21 +1,21 @@
-package dev.slimtom.an_traein.controller;
+// package dev.slimtom.an_traein.controller;
 
-import dev.slimtom.an_traein.model.TrainObservation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import dev.slimtom.an_traein.model.TrainObservation;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-public class TrainObservationController {
+// @RestController
+// public class TrainObservationController {
     
-    @GetMapping("/observations")
-    public List<TrainObservation> getObservations() {
-        return List.of(
-            new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.32"),
-            new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.33"),
-            new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.34"),
-            new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.35")
-        );
-    }
-}
+//     @GetMapping("/observations")
+//     public List<TrainObservation> getObservations() {
+//         return List.of(
+//             new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.32"),
+//             new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.33"),
+//             new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.34"),
+//             new TrainObservation("SLG-DUB-0500", "Sligo MacDiarmada", "Dublin Connolly", "Mullingar", "08.22", "09.00", 38, "08.35")
+//         );
+//     }
+// }
