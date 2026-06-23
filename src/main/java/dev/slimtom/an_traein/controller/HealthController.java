feb546728770399783b239d4af29dc303b4a0a6f
seandurnin-cs <sdurnin144@gmail.ie>
2026-06-23 14:36:59 +0100

    Updated Aplication properties to connect spring to postgres and fixed a bug where the healthcheck file prevented running

--- src/main/java/dev/slimtom/an_traein/controller/HealthController.java
+++ src/main/java/dev/slimtom/an_traein/controller/HealthController.java
@@ -1,4 +1,4 @@
-package dev.slimtom.an_traein;
+package dev.slimtom.an_traein.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
